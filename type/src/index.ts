6f0ec3c74aff7742ea8adcc9357fca2ac31f4b02
nogawanogawa <t_tsuno_tsuno@yahoo.co.jp>
2020-06-21 11:34:38 +0900

    [add] type check sample

---
+++ type/src/index.ts
@@ -0,0 +1,23 @@
+let a = 1042
+console.log(typeof a)
+
+let b = "apple and oranges"
+console.log(typeof b)
+
+const c = "pineapples"
+console.log(typeof c)
+
+let d = [true, true, false]
+console.log(typeof d)
+
+let e = {type: "ficus"}
+console.log(typeof e)
+
+let f = [1, false]
+console.log(typeof f)
+
+let g = [3]
+console.log(typeof g)
+
+let h = null
+console.log(typeof h)
